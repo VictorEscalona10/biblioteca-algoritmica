f56e4807d7490dcac18265f87f3c9d6b77076f2a
VictorAdmin <victorescalona2006@gmail.com>
2025-02-04 20:04:03 -0400

    .

--- login.py
+++ login.py
@@ -6,7 +6,7 @@ def login(page: ft.Page):
         print(user.value, password.value)
         page.go("/segunda")
 
-    title = ft.Text("Iniciar sesion", size=25, text_align=ft.TextAlign.CENTER, color=ft.colors.BLUE)
+    title = ft.Text("Iniciar sesion", size=26, text_align=ft.TextAlign.CENTER, color=ft.colors.BLUE)
     user = ft.TextField(label="Usuario", width=300)
     password = ft.TextField(label="Contraseña ", width=300)
     login = ft.ElevatedButton(
