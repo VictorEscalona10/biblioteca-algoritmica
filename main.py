84b5a23d5b486fd4ade525d93bb1f85543c97f61
SaMH64 <mu086424680@gmail.com>
2025-02-22 21:20:10 -0400

    Se acomodo la interfaz de usuario provisionalmente y se añadio una imagen al inicio de sesión del programa (Yo sé que fue un cambio de mrd)

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from login import login
 from app import app
 #pipeline
 def main(page: ft.Page):
-    page.bgcolor = '#0f1114'
+    page.window_size = (1280, 720)
+    page.window.alwais_on_top = True
+    page.bgcolor = ft.Colors.TRANSPARENT
+    page.decoration = ft.BoxDecoration(image=ft.DecorationImage(src="resources/images/P2210039.JPG" ,fit=ft.ImageFit.COVER, opacity=0.5))
     page.title = "Biblioteca"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
     page.user_authenticated = None
 
     def route_change(e):
@@ -26,4 +28,3 @@ def main(page: ft.Page):
     page.go("/")
         
 ft.app(target=main)
-
