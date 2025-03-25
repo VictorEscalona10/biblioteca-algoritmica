5056c9dffa5b47c6d9be285828bfd664e29a7f2b
Brayan <brayanbracho1515@gmail.com>
2025-03-24 21:35:07 -0400

    se agrego home y los libros en user_profile

--- screens/home.py
+++ screens/home.py
@@ -2,19 +2,21 @@ import flet as ft
 
 def home(page: ft.Page):
     page.window_size = (1280, 720)
-    title = ft.Text("Bienvenido a la Biblioteca", size=34, weight="bold")
-    text = ft.Text("Seleccione una opción del menú", size=15)
+    title = ft.Text("Bienvenido a la Biblioteca", size=60, weight="bold", color="white")
+    text = ft.Text("Seleccione una opción del menú", size=25, color="white")
 
     container = ft.Container(
-        content=ft.Column([title, text]),
-        alignment=ft.alignment.center,
+        content=ft.Column(
+            [title, text],
+            alignment=ft.MainAxisAlignment.CENTER, 
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER, 
+        ),
+        alignment=ft.alignment.center, 
         expand=True,
         bgcolor=ft.colors.BLUE_700,
         border_radius=10,
     )
-
-    return container
-
+    
     
 
-
+    return container
